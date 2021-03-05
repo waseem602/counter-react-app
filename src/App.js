71b9aec2c419731cacccd771552c910ce322f689
MUHAMMAD WASEEM ARIF <wasimarif602@gmail.com>
2021-03-05 11:48:37 +0500

    edited app.css, app.js and added message.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Message} from './Message';
+
 
 function App() {
+  let [count, setCount] = useState(0);
+  let [time, setTime]=useState(true);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={time ? 'dayLight' : 'night'}>
+      <h2>Good {time ? 'Morning':'Night'}</h2>
+    <Message counter={count}/>
+    <br />
+    
+      <button onClick={
+        ()=>setCount(++count)
+        }>
+          Update Counter
+      </button>
+      <button onClick={()=>setTime(!time)}>Update Time</button>
+      
     </div>
   );
 }
---
+++ src/Message.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Message(props){
+    return(
+    <h3>Value in the variable is: {props.counter}</h3>
+    );
+}
